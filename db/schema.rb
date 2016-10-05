8d21661403e5553f74c17c7e2720627b929e6d93
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-05 16:50:52 -0400

    added user show page and feature test

--- db/schema.rb
+++ db/schema.rb
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 20161005150608) do
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
     t.string   "username"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
+
 end
