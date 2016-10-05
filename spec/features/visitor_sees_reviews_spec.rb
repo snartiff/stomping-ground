2c76d31c35d0ee4791776a7fa246f663ff019f6f
anniehck <anniehckim@gmail.com>
2016-10-05 16:59:36 -0400

    Wrote review tests

---
+++ spec/features/visitor_sees_reviews_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'unauthenticated visitor sees reviews' do
+  scenario 'visitor views district reviews' do
+    district = FactoryGirl.create(:district)
+    review = FactoryGirl.create(:review, district_id: district.id)
+
+    visit root_path
+    click_link district.name
+
+    expect(page).to have_content district.name
+    expect(page).to have_content review.title
+    expect(page).to have_content review.body
+    expect(page).to have_content review.rating
+    expect(page).to have_content review.user.username
+  end
+
+  scenario 'visitor fails to add review' do
+    district = FactoryGirl.create(:district)
+
+    visit root_path
+    click_link district.name
+
+    expect(page).to have_content district.name
+
+    click_link 'New Review'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+    expect(page).to have_content 'LOG IN'
+  end
+end
