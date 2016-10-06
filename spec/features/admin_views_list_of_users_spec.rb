2f4e932ac9cc53cd8d86f380a21d7e0ce58d0884
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-06 10:37:06 -0400

    WIP

--- spec/features/admin_views_list_of_users_spec.rb
+++ spec/features/admin_views_list_of_users_spec.rb
@@ -5,12 +5,14 @@ feature 'admin views users' do
   scenario 'an admin views list of all users' do
     admin = User.create(username: "Jewls", first_name: "Julie", last_name: "Grace", email: "hello@test.com", password: "password", password_confirmation: "password", role: "admin")
 
-    user = FactoryGirl.create(:user)
+    users = FactoryGirl.create_list(:user, 5)
 
     visit root_path
     click_link("All Users")
 
     expect(page).to have_content("All Users")
-    expect(page).to have_content(user.username)
+    users.each do |user|
+      expect(page).to have_content(user.username)
+    end
   end
 end
