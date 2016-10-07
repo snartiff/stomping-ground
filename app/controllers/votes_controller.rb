fe49dca3b1bb0a807024a0e0c3270afc430eac9a
Kyle Wood <woodk53@gmail.com>
2016-10-07 14:25:20 -0400

    Added vote functionality without ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,33 @@ class VotesController < ApplicationController
 
   def vote_up
     @review = Review.find(params[:id])
-    @vote = @review.votes.create(:user_id => current_user.id, :review_id => @review.id, :vote => true)
+    @votes = @review.votes
+    if @votes.exists?(:user => current_user)
+      @vote = @votes.where(:user => current_user)[0]
+      if !@vote.vote
+        @vote.update_attribute(:vote, true)
+      else
+        Vote.destroy(@vote)
+      end
+    else
+      @review.votes.create(:user_id => current_user.id, :review_id => @review.id, :vote => true)
+    end
+
   end
 
   def vote_down
     @review = Review.find(params[:id])
-    @vote = @review.votes.create(:user_id => current_user.id, :review_id => @review.id, :vote => false)
+    @votes = @review.votes
+    if @votes.exists?(:user => current_user)
+      @vote = @votes.where(:user => current_user)[0]
+      if @vote.vote
+        @vote.update_attribute(:vote, false)
+      else
+        Vote.destroy(@vote)
+      end
+    else
+      @review.votes.create(:user_id => current_user.id, :review_id => @review.id, :vote => false)
+    end
   end
 
 end
