3ffa4a5410a3db5c1b73d6760e4a8dcd580ddb3a
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-11 14:32:31 -0400

    disabled actionmailer

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,7 +21,8 @@ class ReviewsController < ApplicationController
     @review.user_id = current_user.id
 
     if @review.save
-      ReviewMailer.new_review(@review).deliver_now
+      # disabled so heroku will save a review
+      # ReviewMailer.new_review(@review).deliver_now
       flash[:success] = "Review added successfully"
       redirect_to district_path(@review.district)
     else
