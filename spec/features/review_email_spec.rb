f0270b7cb1f53f3f61707a14fd3abe716e3515bc
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-11 11:46:16 -0400

    removed pry and launchy

--- spec/features/review_email_spec.rb
+++ spec/features/review_email_spec.rb
@@ -22,8 +22,6 @@ feature 'email sent for added review' do
     expect(page).to have_content("Look at how I turned up")
     expect(page).to have_content("Rating: 1")
     expect(page).to have_content("Reviewer: #{user.username}")
-    save_and_open_page
-    binding.pry
     expect(ActionMailer::Base.deliveries.count).to eq(1)
   end
 end
