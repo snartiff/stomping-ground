08435c550a038708efb5981b8160844a0421ffcb
Kyle Wood <woodk53@gmail.com>
2016-10-12 09:35:31 -0400

    Added edits to districts and reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,44 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-    review = Review.find(params[:id])
-    if signed_in? && (current_user == review.user || current_user.admin)
-      @district = District.find(params[:district_id])
-      @review = Review.find(params[:id])
-    elsif !signed_in?
-      authenticate_user!
-    else
-      flash[:alert] = 'You have to sign in first!'
-      redirect_to district_path(@district)
-    end
-  end
-
-  def update
-    @review = Review.find(params[:id])
-    @district = District.find(params[:district_id])
-    if signed_in? && (current_user == @review.user || current_user.admin)
-      if @review.update_attributes(review_params)
-        flash[:notice] = 'Review successfully updated.'
-        redirect_to district_path(@district)
-      elsif !signed_in?
-        authenticate_user!
-      else
-        flash[:error] = @review.errors.full_messages.join(", ")
-        render :edit
-      end
-    else
-      flash[:alert] = 'You have no permission to edit this review'
-      redirect_to district_path(@district)
-    end
-  end
-
   def new
     @review = Review.new
     @district = District.find(params[:district_id])
@@ -69,9 +31,24 @@ class ReviewsController < ApplicationController
     redirect_to district_path(@district)
   end
 
+  def edit
+    @district = District.find(params[:district_id])
+    @review = @district.reviews.find(params[:id])
+  end
+
+  def update
+    @district = District.find(params[:district_id])
+    @review = @district.reviews.find(params[:id])
+    if @review.update_attributes(review_params)
+      redirect_to @district
+    else
+      render :edit
+    end
+  end
+
   protected
 
-  # def review_params
-  #   params.require(:review).permit(:title, :body, :rating)
-  # end
+  def review_params
+    params.require(:review).permit(:title, :body, :rating)
+  end
 end
