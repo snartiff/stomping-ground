b9a9af9291f16b16f9fe5b27b5ae99c1974b888c
Julie Graceffa <julie.graceffa@gmail.com>
2016-10-12 16:09:53 -0400

    added up vote and down vote buttons and tested

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   root "districts#index"
+  get "votes/:id/vote_up" => "votes#vote_up", as: "vote_up"
+  get "votes/:id/vote_down" => "votes#vote_down", as: "vote_down"
 
   resources :districts do
     resources :reviews, except: [:show, :index]
