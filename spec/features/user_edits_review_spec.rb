daa09f68893a8b766397b1c2e44262ad09f77682
Kyle Wood <woodk53@gmail.com>
2016-10-13 14:08:20 -0400

    Fixed syntax errors for editing reviews and districts. Updated tests accordingly

--- spec/features/user_edits_review_spec.rb
+++ spec/features/user_edits_review_spec.rb
@@ -35,3 +35,28 @@ feature 'authenticated user can edit review' do
     expect(page).to have_content 'I changed my review'
   end
 end
+
+feature 'authenticated user leaves edit field blank' do
+  scenario 'user leaves body and rating blank' do
+    user = FactoryGirl.create(:user)
+    district = FactoryGirl.create(:district)
+    review = FactoryGirl.create(:review, district_id: district.id, user_id: user.id)
+
+    user_signs_in(user)
+    visit root_path
+    click_link district.name
+    expect(page).to have_content district.name
+    expect(page).to have_content 'Lived here my whole life and it\'s been amazing'
+    expect(page).to have_content 'Title: Best neighborhood ever'
+
+    click_link 'Edit Review'
+    fill_in 'review_title', with: ''
+    fill_in 'review_body', with: ''
+    fill_in 'review_rating', with: ''
+    click_button 'Submit'
+    expect(page).to have_content '3 errors prohibited this review from being saved:'
+    expect(page).to have_content 'Title can\'t be blank'
+    expect(page).to have_content 'Body can\'t be blank'
+    expect(page).to have_content 'Rating is not a number'
+  end
+end
