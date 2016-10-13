e8677d1c309e44802ec64d4a5af03ed2e76588da
anniehck <anniehckim@gmail.com>
2016-10-13 13:39:18 -0400

    Fixed styling

--- react/src/components/District.js
+++ react/src/components/District.js
@@ -4,8 +4,8 @@ const District = props => {
   let path = `districts/${props.id}`;
   let image = props.picture;
   return (
-    <li key={props.id}>
-      <img src={image} /><br />
+    <li key={props.id} className="neighborhood">
+      <a href={path}><img src={image} className="district-image"/></a><br />
       <a href={path}>{props.name}</a>
     </li>
   );
